186e518e9c7cb62ee25d7966a282a82a330a1eb9
jose.guevara <134351019+JPGuevara37@users.noreply.github.com>
2023-12-29 23:30:36 -0600

    Juego de dados y premios

--- Program.cs
+++ Program.cs
@@ -20,24 +20,36 @@ Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
+        Console.WriteLine("Oh vaya que suerte! Felicitaciones tiraste triple, ahora tienes x3 bonus al total!");
+        total *= 3;
+        Console.WriteLine($"Ahora obtienes {total}");
+    } 
+    else
+    {
     Console.WriteLine("Tiraste con dobles por lo que se x2 bonus al Total!");
     total *= 2;
     Console.WriteLine($"Ahora obtienes {total}");
+    }
 }
 
-if ((roll1 == roll2) && (roll2 == roll3)) 
+
+
+if (total >= 40)
 {
-    Console.WriteLine("Oh vaya que suerte! Felicitaciones tiraste triple, ahora tienes x3 bonus al total!");
-    total *= 3;
-    Console.WriteLine($"Ahora obtienes {total}");
+    Console.WriteLine("Ganaste un auto nuevo");
 }
 
-if (total > 14)
+else if (total >= 20)
 {
-    Console.WriteLine("Ganaste");
+    Console.WriteLine("Ganaste una computadora nueva");
 }
-
+else if (total >= 10)
+{
+    Console.WriteLine("Ganaste un pan");
+}
 else
 {
-    Console.WriteLine("Perdiste");
+    Console.WriteLine("Premio de consolacion ganaste un gatito");
 }
