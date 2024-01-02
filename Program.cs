92a87f87a028ccf2bc8d764eb8d16901382345e6
jose.guevara <134351019+JPGuevara37@users.noreply.github.com>
2024-01-02 08:50:15 -0600

    Proyecto

--- Program.cs
+++ Program.cs
@@ -88,3 +88,173 @@ else if (daysUntilExpiration <= 10)
     Console.WriteLine("Tu subscripcion expirara pronto, por favor renuevala");
 }
 */
+
+//Arreglos
+/*
+string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+// mal: fraudulentOrderIDs[3] = "D000";
+*/
+/*
+string[] fraudulentOrderIDs = {"A123","B456","C789"};
+
+Console.WriteLine($"Primero: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Segundo: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Tercero: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reasignar numero: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+
+string[] names = { "Rowena", "Robin", "Bao" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+*/
+
+/*
+using System.Runtime.CompilerServices;
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Categoria {bin} = {items} de articulos (En total hay: {sum})");
+    
+}
+
+Console.WriteLine($"Tenemos {sum} de articulos en inventario de {inventory.Length} categorias diferentes");
+*/
+
+/* Hacer un recorrido para extraer el arreglo los que empiezan por B
+
+string[] elements = {"B123","C234","A345","C15","B177","G3003","C235","B179"};
+
+string pedidos = "";
+
+foreach(string element in elements)
+{
+    pedidos = element;
+
+    if (pedidos.StartsWith("B"))
+    {
+        Console.WriteLine($"The {pedidos} fraudes!!!");
+    }
+    
+}
+*/
+
+/*
+using System;
+
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int sophia1 = 90;
+int sophia2 = 86;
+int sophia3 = 87;
+int sophia4 = 98;
+int sophia5 = 100;
+
+int andrew1 = 92;
+int andrew2 = 89;
+int andrew3 = 81;
+int andrew4 = 96;
+int andrew5 = 90;
+
+int emma1 = 90;
+int emma2 = 85;
+int emma3 = 87;
+int emma4 = 98;
+int emma5 = 68;
+
+int logan1 = 90;
+int logan2 = 95;
+int logan3 = 87;
+int logan4 = 88;
+int logan5 = 96;
+
+int sophiaSum = 0;
+int andrewSum = 0;
+int emmaSum = 0;
+int loganSum = 0;
+
+decimal sophiaScore;
+decimal andrewScore;
+decimal emmaScore;
+decimal loganScore;
+
+sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
+emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
+loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
+
+sophiaScore = (decimal)sophiaSum / currentAssignments;
+andrewScore = (decimal)andrewSum / currentAssignments;
+emmaScore = (decimal)emmaSum / currentAssignments;
+loganScore = (decimal)loganSum / currentAssignments;
+
+Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
+Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
+Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
+*/
+
+string[] estudiantes = {"sophia","Andrew","Emma","Logan"};
+ 
+decimal[] notasSophia = {90,86,87,98,100};
+decimal[] notasAndrew = {92,89,81,96,90};
+decimal[] notasEmma = {90,85,87,98,68};
+decimal[] nortasLogan = {90,95,87,88,96};
+
+
+int sumSophia = 0;
+int sumAndrew = 0;
+int sumEmma= 0;
+int sumLogan = 0;
+
+
+
+Console.WriteLine("Student\t\tGrade");
+//double totalSophia = 0;
+
+int totalDeAsignaturas = 5;
+
+    foreach (string nombres in estudiantes)
+    {
+        Console.WriteLine($"{nombres}");
+    }
+
+
+ /*if (sumSophia >= 90 && sumSophia < 93)
+    {
+        Console.WriteLine ($"Sophia:\t\t{sumSophia}\t A-");
+    }
+    else {
+
+    }*/
+
+
+
+
+
+
+
+
+
+
